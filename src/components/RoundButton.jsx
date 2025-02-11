f14ec98604b90b20f0b71f7c532f79e1333cc031
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2025-02-11 22:25:58 +0500

    Updates

--- src/components/RoundButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function RoundButton({text, className, onClick, disabled}) {
-  return (
-    <button onClick={onClick} disabled={disabled} className={`flex justify-center items-center border bg-gray-300 shadow-md md:w-12 md:h-12 w-8 h-8 text-center place-content-center text-nowrap opacity-50 rounded-full md:text-2xl text-xl z-10 border-gray-400/25 active/:bg-gray-600 hover:shadow-md cursor-pointer ${className} transition-all duration-300 ease-out `}>{text}</button>
-  )
-}
-
-export default RoundButton
